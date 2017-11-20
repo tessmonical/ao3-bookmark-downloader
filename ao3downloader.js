2cf3f4f15124ee233ada323a441b69c3427b491c
Therese Monical <omnomnomtea@users.noreply.github.com>
2017-11-19 18:52:20 -0600

    Add files via upload

---
+++ ao3downloader.js
@@ -0,0 +1,14 @@
+
+
+//matches parts of a page of the form <a href="/works/00000000" where the zeroes are the work number
+const regexp = /<a href="\/works\/(\d+)">/g;
+const username = 'resonance_and_d';
+const currentPage = 1;
+
+//gets the first page of the bookmarks
+axios.get(`http://archiveofourown.org/users/${username}/bookmarks?page=${currentPage}`)
+  .then(html_page => {
+    console.log(html_page);
+  })
+
+
